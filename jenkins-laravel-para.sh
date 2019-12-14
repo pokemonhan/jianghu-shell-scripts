03343e0bfa1237f96ebc14956d76c2903c06811b
Harris <harrisdt15f@gmail.com>
2019-12-14 13:40:03 +0800

    :tada: update with module

--- jenkins-laravel-para.sh
+++ jenkins-laravel-para.sh
@@ -40,7 +40,7 @@ echo
 destination_user="$dest_user_staging"
 destination_host="$dest_host_staging" #$dest_host_staging　
 destination_dir="$dest_dir_staging"
-gitlabip="172.19.0.14" # 172.22.0.1　9170ttt.com
+gitlabip="172.19.0.1" # 172.22.0.1　9170ttt.com
 ##################################################################################
 case $Status  in
   Deploy)
@@ -84,13 +84,29 @@ case $Status  in
                 echo \"REMOTE is \$REMOTE\";\
               if [ \$LOCAL != \$REMOTE ] || [ -z \$REMOTE ]
 then
-              rm -rf composer.lock;
               git reset --hard;
               git fetch --all;
               git checkout -f $destination_branch;
               git reset --hard;
               git fetch --all;
               git pull origin $destination_branch;
+              cat > ${destination_dir}/.gitmodules <<EOL
+[submodule \"phpcs-rule\"]
+    path = phpcs-rule
+    url = ssh://git@${destination_host}:2289/php/phpcs-rule.git
+EOL
+        chmod 777 ${destination_dir}/.gitmodules;
+        if [[ ! -e ${destination_dir}/phpcs-rule ]]; then
+            git submodule add -f ssh://git@${destination_host}:2289/php/phpcs-rule.git phpcs-rule;
+        fi;
+        git submodule init;
+        git submodule sync;
+        cd ${destination_dir}/phpcs-rule;
+        git -c credential.helper= -c core.quotepath=false -c log.showSignature=false checkout master --;
+        git -c credential.helper= -c core.quotepath=false -c log.showSignature=false fetch origin --progress --prune;
+        git pull origin master;
+        cd $destination_dir;
+              rm -rf composer.lock;
               git tag -l | xargs git tag -d && git fetch -t;
               /usr/local/bin/composer install --no-interaction --no-progress --no-ansi --prefer-dist --optimize-autoloader;
               php artisan clear-compiled;
