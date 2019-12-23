68cb6facace5a75fae7252dcbf2411b16db4e06c
harris <harrisdt15f@gmail.com>
2019-12-23 13:18:27 +0800

    update sending message completement

--- jenkins-laravel-para.sh
+++ jenkins-laravel-para.sh
@@ -135,8 +135,9 @@ EOL
               cd /var/www/telegram-bot-bash;\
               export BASHBOT_HOME=\"\$(pwd)\";\
               source ./bashbot.sh source;\
-              telegrammsg=\"🤩🤩🤩🤩🤩🤩 [ 已发布版本：$version_prefix-$BUILD_NUMBER ] 🤩🤩🤩🤩🤩🤩 \n\n发布摘要: \${message} \";\
-              send_message $tg_chat_group_id \${telegrammsg};\
+              startEmoji=\"🤩🤩🤩🤩🤩🤩\";\
+              telegrammsg=\$startEmoji\"[ 已发布版本:$version_prefix-$BUILD_NUMBER ]\$startEmoji\n\n[ 发布摘要:\$message ]\";\
+              send_message $tg_chat_group_id \"\$telegrammsg\";\
 else\
     echo \"Nothing to do\";
 fi;\
