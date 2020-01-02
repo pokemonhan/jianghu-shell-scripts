74f5a649c528d5edcd6d009a2d661240983b1b5f
Harris <harrisdt15f@gmail.com>
2020-01-02 17:36:31 +0800

    :tada: cannot found centos command solve

---
+++ deletetag.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+function handleTagDate()
+{
+    local line="$1"
+	local tagName=$(echo "$line"| cut -d '|' -f1)
+	local date=$(echo "$line"| cut -d '|' -f2)
+	#90 days ago
+	local timeago='2 days ago'
+	local dtSec=$(date --date "$date" +'%s')
+	local taSec=$(date --date "$timeago" +'%s')
+	# echo "INFO: dtSec=$dtSec, taSec=$taSec" >&2
+	if [[ $dtSec -lt $taSec ]]; then
+		echo "$line alreay 2 days ago"
+		git tag -d "$tagName"
+		git push --delete origin "$tagName"
+	else
+		echo "$line can live more"
+	fi
+	# echo "now is at $line" & echo "current tagName is $tagName and current date is $date"
+}
+cd /e/projects/jianghu_entertain
+listsTag=$(git for-each-ref --sort=taggerdate --format '%(refname:short)|%(taggerdate:short)' refs/tags | egrep -v "(^\*|release*)")
+for line in $listsTag
+do
+  ##############[ Date time clean Tag ]#############
+  handleTagDate "$line" &
+  ####################################################
+done
+###################################################
+echo Press Enter...
+read
