748b693a325edb36483e068e58a24453c6fed4be
harris <harrisdt15f@gmail.com>
2020-01-13 23:23:41 +0800

    artisan command should done after composer

--- jenkins-local-deploy.sh
+++ jenkins-local-deploy.sh
@@ -206,8 +206,8 @@ fi
                           -p 2225                         \
                           -i /var/jenkins_workspace/harrisdock/workspace/insecure_id_rsa    \
                          "cd $destination_dir;\
-                         $ActionCommand;\
                          bash /var/www/$currentScriptDir/localDeploy/git/git-submodule-composer-update.sh $destination_dir; \
+                         $ActionCommand;\
                          /usr/local/bin/composer dump-autoload;\
                          chmod -R 777 ${destination_dir}/storage;\
                          chmod -R 777 ${destination_dir}/vendor;\
