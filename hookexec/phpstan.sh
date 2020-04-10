23b26d0cbe352b02a4ea169dc7fa168a062f3e3a
Harris <harrisdt15f@gmail.com>
2020-04-10 23:56:11 +0800

    :tada: update phpstan hooks

--- hookexec/phpstan.sh
+++ hookexec/phpstan.sh
@@ -3,5 +3,6 @@ neonfile="$2"
 autoloadPath="$3"
 changed_file="$4"
 cd $projDir;
+composer dump-autoload;
 echo "now phpstan execute command is $projDir/vendor/bin/phpstan analyse -c $neonfile -a $autoloadPath $changed_file --error-format=table --memory-limit=1G"
 $projDir/vendor/bin/phpstan analyse -c $neonfile -a $autoloadPath $changed_file --error-format=table --memory-limit=1G
