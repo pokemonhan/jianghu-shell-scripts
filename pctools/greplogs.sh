ea82cf0110c1925f052d23dac407f81c7e32f631
Harris <harrisdt15f@gmail.com>
2020-09-26 22:28:07 +0800

    update grep file for selected words

---
+++ pctools/greplogs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+dir=/d/log/;
+word="sendCode: error:"
+FILES="$dir*"
+for f in $FILES
+do
+  # echo "Processing $f file..."
+  out="$(basename $f)"
+  echo "Filename is $out"
+  grep -i -e "$word" $f > "$dir$out-Selected.log";
+  # take action on each file. $f store current file name
+done
+echo Press Enter...
+read
